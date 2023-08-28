62d6bdb886db74a53ef986744c129d2e53af20b9
Michael Yang <michael83988@gmail.com>
2023-08-28 15:45:57 +0800

    2323-08-28 Upload file.

--- American_Stock_Find_Target.py
+++ American_Stock_Find_Target.py
@@ -17,20 +17,37 @@ def get_historic_price(abbr, playwright, avg_type):
 
 
     # 找到輸入公司簡稱的input
-    page.get_by_placeholder("Quote Lookup").type(abbr, delay = 2000)
+    page.get_by_placeholder("Quote Lookup").type(abbr, delay = 3000 + np.random.rand() * 1000)
     page.keyboard.press("Enter", delay = np.random.rand() * 1000 + 1000)   
 
     
     # 取得跳轉該公司頁面後的公司名稱
     com_name_selector = "#quote-header-info > div.Mt\(15px\).D\(f\).Pos\(r\) > div.D\(ib\).Mt\(-5px\).Maw\(38\%\)--tab768.Maw\(38\%\).Mend\(10px\).Ov\(h\).smartphone_Maw\(85\%\).smartphone_Mend\(0px\) > div.D\(ib\) > h1"
-    company_name = page.wait_for_selector(com_name_selector, state = "attached", timeout = 30000)    
+    company_name = page.wait_for_selector(com_name_selector, state = "attached", timeout = 30000)  
+
+
+
     print("Get targeted company completely!")  
     print(company_name.inner_text())       
 
 
     # 點hisotorical data
-    type_selector = "#quote-nav > ul > li.IbBox.Fw\(500\).fin-tab-item.H\(44px\).desktop_Bgc\(\$hoverBgColor\)\:h.desktop-lite_Bgc\(\$hoverBgColor\)\:h.selected > a > span"
-    page.wait_for_selector(type_selector, state = "attached", timeout = 30000).click(timeout = 5000, delay = np.random.rand() * 1000)
+    type_selector = "#quote-nav > ul > li.IbBox.Fw\(500\).fin-tab-item.H\(44px\).desktop_Bgc\(\$hoverBgColor\)\:h.desktop-lite_Bgc\(\$hoverBgColor\)\:h.selected > a > span"           
+    #quote-nav > ul > li.IbBox.Fw\(500\).fin-tab-item.H\(44px\).desktop_Bgc\(\$hoverBgColor\)\:h.desktop-lite_Bgc\(\$hoverBgColor\)\:h.selected > a > span
+    # page.wait_for_selector(type_selector, state = "attached", timeout = 30000).click(timeout = 5000, delay = np.random.rand() * 1000)
+    page.get_by_text("Historical Data").click(timeout = 5000, delay = np.random.rand() * 1000)
+    print("點歷史資料")
+
+    # 點Frequency: Monthly
+    page.get_by_text("Daily").click(timeout = 5000, delay = np.random.rand() * 1000)
+    menu = page.locator("data-test=historicalFrequency-menu")
+    page.locator("data-value=1mo").click(timeout = 5000, delay = np.random.rand() * 1000)
+    page.get_by_text("Apply").click(timeout = 5000, delay = np.random.rand() * 1000)
+    page.screenshot(path = "screenshot1.png")
+    print("OK")
+
+    # current_url = page.url
+    # print(menu.inner_html())
 
 
 
